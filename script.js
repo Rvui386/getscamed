0405ee6bd7586a438b2e4e41e9b4aa009b26728b
Rvui386 <hamza.m.siddiqui25@gmail.com>
2025-11-05 08:01:27 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const Notifybtn = document.querySelector("scam");
+function scam(){
+    if(!("Notification" in window)){
+        throw new Error("Your browser does not support push notification");
+    }
+    Notification.requestPermission().then((Permission)=>{
+        const notificationOptions = {
+            body:"Oh no! Your computer got hacked!",
+        }
+        new Notification("Push Notification",notificationOptions);
+    })
+};
