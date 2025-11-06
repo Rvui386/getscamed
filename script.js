0087d56d3fe786fd77af46a7450f3abef2919733
Rvui386 <hamza.m.siddiqui25@gmail.com>
2025-11-06 13:02:24 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 const Notifybtn = document.querySelector("scam");
 function scam(){
-    
     if(!("Notification" in window)){
         throw new Error("Your browser does not support push notification");
     }
     Notification.requestPermission().then((Permission)=>{
-        while (true){
-            const notificationOptions = {
-                body:"Oh no! Your computer got hacked by user 0xF89028!",
-            }
-            new Notification("Error:",notificationOptions);
+        const notificationOptions = {
+            body:"Oh no! Your computer got hacked!",
         }
-});
+        while(true){
+        new Notification("Push Notification",notificationOptions);
+        }
+    })
 };
